3f11ca1ac7ff50604c9ea855c5175b11eb353e7e
Popczak, Arkadiusz <arkadiusz.popczak@aptiv.com>
2018-01-04 13:02:32 +0100

    Latest changes

--- functions.py
+++ functions.py
@@ -5,4 +5,5 @@ def gauss(x, *p):
     a, mu, sigma = p
     return a * np.exp((-(x-mu)**2)/(2.*sigma**2))
 
-
+def map_an(x, in_min, in_max, out_min, out_max):
+    return ((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
